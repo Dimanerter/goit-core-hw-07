7b8aa6c577351265fd9898f7803c9fcccba0e87e
Dmitriy Avdeev <91690783+Dimanerter@users.noreply.github.com>
2024-11-17 14:38:12 +0100

    Update task.py

--- task.py
+++ task.py
@@ -27,7 +27,7 @@ class Birthday(Field):
     def __init__(self, value):
         try:
             if datetime.strptime(value, "%d.%m.%Y"):
-             super().__init__(value)
+            super().__init__(value)
         except ValueError:
             raise ValueError("Invalid date format. Use DD.MM.YYYY")
 
